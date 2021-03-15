5a5884a4c3109d02a3fbe95bead6e9ea691237e9
CaioMassayuki <japa.zito@hotmail.com>
2021-03-15 14:53:12 -0300

    SpeedReading v0

--- script.js
+++ script.js
@@ -3,16 +3,26 @@
   const _reader = document.getElementById('reader')
   const _book = document.getElementById('book')
   const _btnStart = document.getElementById('startReading')
+  const _btnStop = document.getElementById('stopReading')
 
 
   let partBook = []
+  let readingTime
   _btnStart.addEventListener('click', () => {
     const fullBook = _book.value
     partBook = fullBook.split(/\s/gim)
+    readingTime = setInterval(() => {
+      console.log(partBook[0])
+      if(partBook.length !== 0){
+        _reader.textContent = partBook.shift()
+      } else {
+        clearInterval(readingTime)
+      }
+    }, 200)
   })
 
-  const readingTime = setInterval(() => {
-    _reader.textContent = partBook.shift()
-  }, 200)
+  _btnStop.addEventListener('click', () => {
+    clearInterval(readingTime)
+  })
   
 })()
