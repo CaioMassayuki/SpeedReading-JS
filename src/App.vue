a01a79f09c1aa89415fbc3f3626fefcf6a34c6fd
Victor Hirumitsu <vmhirumitsu@gmail.com>
2021-03-15 23:51:31 -0300

    First draft

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,43 @@
 <template>
   <div id="app">
+    <div>
+      <span>_</span>
+      <text-reader :raw-text="text"/>
+    </div>
   </div>
 </template>
 
 <script>
+import TextReader from '@/components/TextReader'
+
+const text = `1
+12
+123
+1234
+12345
+123456
+1234567
+12345678
+123456789
+1234567890
+12345678901
+123456789012
+1234567890123
+12345678901234
+123456789012345
+1234567890123456
+12345678901234567
+123456789012345678
+1234567890123456789`
+
 export default {
   name: 'App',
+  components: {TextReader},
+  computed: {
+    text() {
+      return text;
+    }
+  },
 }
 </script>
 
@@ -14,7 +46,24 @@ body
   margin 0
 
 #app
-  font-family Avenir, Helvetica, Arial, sans-serif
+  font-family Consolas, monospace
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
+  width 100%
+  min-height 100vh
+  background-color sr-black
+  color sr-white
+  display flex
+  justify-content center
+  align-items center
+
+  > div
+    width 60rem
+
+    > span
+      display block
+      width 100%
+      text-align center
+      line-height 1
+      font-size 80px
 </style>
---
+++ src/components/TextReader.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    {{/* Magic numbers. Don't touch, don't question. */}}
+    <svg viewBox="0 0 167 12">
+      <text :x="8.78 * (19 - words[currentWord].length) / 2" y="11.2">{{words[currentWord]}}</text>
+    </svg>
+    <ul>
+      <li @click="startStop">{{started ? 'PAUSE' : 'PLAY'}}</li>
+      <li>SPEED SLIDER TBD</li>
+      <li @click="() => {this.showText = !this.showText}">{{showText ? 'HIDE TEXT' : 'SHOW TEXT'}}</li>
+    </ul>
+    <p v-if="showText">
+      <template v-for="(w, i) of words">
+        <span :key="2 * i" v-if="i !== 0">{{'\n' /* Uma gambiarra em seu habitat natural */ }}</span>
+        <span :key="2 * i + 1" :class="i === currentWord ? 'highlight' : ''">{{w}}</span>
+      </template>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextReader',
+  props: {
+    rawText: String,
+  },
+  computed: {
+    words() {
+      return this.rawText.split(/\s+/)
+    },
+  },
+  data() {
+    return {
+      currentWord: 0,
+      started: false,
+      interval: 0,
+      millisPerWord: 200,
+      showText: false,
+    }
+  },
+  methods: {
+    startStop() {
+      if (this.started) {
+        this.started = false
+        clearInterval(this.interval)
+        return
+      }
+
+      this.started = true
+      this.interval = setInterval(() => {
+        this.currentWord++
+        if (this.words.length === this.currentWord) {
+          this.startStop()
+          this.currentWord = 0
+        }
+      }, this.millisPerWord)
+    },
+  },
+  destroyed() {
+    if (this.started) {
+      clearInterval(this.interval)
+    }
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+div
+  display flex
+  flex-direction column
+  width 100%
+
+  svg
+    background-color sr-word-box-bg-color
+
+    text
+      font-size 16px
+      fill sr-word-box-color
+
+  ul
+    display flex
+    justify-content space-between
+    margin 0
+    padding 0
+
+    li
+      display block
+      margin 0
+      background-color sr-white
+      color sr-black
+      min-width 10%
+      text-align center
+
+  .highlight
+    color sr-highlight-color
+</style>
