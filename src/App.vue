d7651d8b03b9e2526a355454d58d895f979c44ed
Victor Hirumitsu <vmhirumitsu@gmail.com>
2021-04-27 23:42:50 -0300

    Second draft

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <div>
-      <span>_</span>
       <text-reader :raw-text="text"/>
     </div>
   </div>
@@ -10,34 +9,17 @@
 <script>
 import TextReader from '@/components/TextReader'
 
-const text = `1
-12
-123
-1234
-12345
-123456
-1234567
-12345678
-123456789
-1234567890
-12345678901
-123456789012
-1234567890123
-12345678901234
-123456789012345
-1234567890123456
-12345678901234567
-123456789012345678
-1234567890123456789`
-
 export default {
   name: 'App',
   components: {TextReader},
-  computed: {
-    text() {
-      return text;
+  data() {
+    return {
+      text: '',
     }
   },
+  mounted() {
+    fetch('/text.txt').then(res => res.text().then(text => this.text = text))
+  }
 }
 </script>
 
--- src/components/TextReader.vue
+++ src/components/TextReader.vue
@@ -4,14 +4,29 @@
     <svg viewBox="0 0 167 12">
       <text :x="8.78 * (19 - words[currentWord].length) / 2" y="11.2">{{words[currentWord]}}</text>
     </svg>
+    <hr>
     <ul>
-      <li @click="startStop">{{started ? 'PAUSE' : 'PLAY'}}</li>
-      <li>SPEED SLIDER TBD</li>
-      <li @click="() => {this.showText = !this.showText}">{{showText ? 'HIDE TEXT' : 'SHOW TEXT'}}</li>
+      <li>
+        <button @click="startStop">{{started ? 'PAUSE' : 'PLAY'}}</button>
+      </li>
+      <li>
+        <ul>
+          <li>
+            <button @click="slower">SLOWER</button>
+          </li>
+          <li><span>{{wordsPerMinute}} wpm</span></li>
+          <li>
+            <button @click="faster">FASTER</button>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <button @click="() => {this.showText = !this.showText}">{{showText ? 'HIDE TEXT' : 'SHOW TEXT'}}</button>
+      </li>
     </ul>
     <p v-if="showText">
       <template v-for="(w, i) of words">
-        <span :key="2 * i" v-if="i !== 0">{{'\n' /* Uma gambiarra em seu habitat natural */ }}</span>
+        <span :key="2 * i" v-if="i !== 0">{{'\n' /* Uma gambiarra em seu habitat natural */}}</span>
         <span :key="2 * i + 1" :class="i === currentWord ? 'highlight' : ''">{{w}}</span>
       </template>
     </p>
@@ -34,7 +49,7 @@ export default {
       currentWord: 0,
       started: false,
       interval: 0,
-      millisPerWord: 200,
+      wordsPerMinute: 300,
       showText: false,
     }
   },
@@ -53,7 +68,21 @@ export default {
           this.startStop()
           this.currentWord = 0
         }
-      }, this.millisPerWord)
+      }, 60000 / this.wordsPerMinute)
+    },
+    faster() {
+      this.wordsPerMinute++
+      if (this.started) {
+        this.startStop()
+        this.startStop()
+      }
+    },
+    slower() {
+      this.wordsPerMinute--
+      if (this.started) {
+        this.startStop()
+        this.startStop()
+      }
     },
   },
   destroyed() {
@@ -69,6 +98,10 @@ div
   display flex
   flex-direction column
   width 100%
+
+  hr
+    width calc(100% - 4px)
+    border 2px solid sr-grey
 
   svg
     background-color sr-word-box-bg-color
@@ -90,6 +123,21 @@ div
       color sr-black
       min-width 10%
       text-align center
+
+      span
+        width 5rem
+        height 100%
+        padding 0 1rem
+        background-color sr-grey
+
+      button
+        display block
+        border-radius 0
+        border unset
+        background-color sr-white
+        padding 0 1rem
+        width 100%
+        height 100%
 
   .highlight
     color sr-highlight-color
